d130db7f94ae1841fd11eb61c35810985a932c06
Ryan V <ryan.h.vasquez1@gmail.com>
2023-11-16 02:39:44 -0800

    Set up test area and char

--- Scenes/main_charater.gd
+++ Scenes/main_charater.gd
@@ -12,14 +12,13 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	#Animations 
 	if (velocity.x > 1 || velocity.x < -1):
-		sprite_2d.animation = "Run"
-	else:
-			if velocity.x.
-				sprite_2d.animation = "Idle"
+		sprite_2d.animation = "running"
+
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		sprite_2d.animation = "Jumping"
+		sprite_2d.animation = "jumping"
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
